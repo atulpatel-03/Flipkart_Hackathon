672bc04e8bfbe34d8783ee4a2894a5b460b7a45a
atul-code-coder <atulpatel22777@gmail.com>
2021-06-25 17:27:31 +0530

    frontend and backend connected

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div>happy birthday lendu !!</div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+connectDB();
+
 app.get('/',(req,res) =>  res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
