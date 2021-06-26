d731829c66235eafdd4aefa84ccd7c028348c8a0
atul-code-coder <atulpatel22777@gmail.com>
2021-06-26 08:31:05 +0530

    register a user and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import Register from './component/auth/Register';
+
 
 function App() {
   
   return (
    <div>happy birthday lendu !!
     
-    <Register />
+    
    </div>
   );
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,93 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const {OAuth2Client} = require('google-auth-library');
 
-router.get('/', (req,res) => res.send('Auth route'));
+const User = require('../../models/User');
+
+const client = new OAuth2Client("645485184858-lpmdq3q6nefhpt68lj437bmt0v49vv0m.apps.googleusercontent.com");
+
+router.get('/', auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+    
+  });
+
+
+router.post(
+    '/',
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        let user = await User.findOne({ email });
+  
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+  
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: '5 days' },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    }
+  );
+  
+
+router.post('/googlelogin', async (req,res) => {
+   try {
+    const { tokenId }= req.body;
+    
+   const res =await client.verifyIdToken({idToken: tokenId, audience: "645485184858-lpmdq3q6nefhpt68lj437bmt0v49vv0m.apps.googleusercontent.com"})
+    if(res){
+        console.log(res);
+    }
+
+   } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+   }
+});
 
 module.exports = router;
